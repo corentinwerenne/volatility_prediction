b43fafe140f2d8465cf9ee05736c59d4695ffe40
Corentin Werenne <corentinwerenne@gmail.com>
2022-04-13 10:36:33 +0200

    Add get_adj_close function

--- data_handler.py
+++ data_handler.py
@@ -7,3 +7,17 @@ Created on Wed Apr 13 10:08:22 2022
 """
 
 import yfinance as yf
+
+def get_adj_close(ticker_list, start, end, interval):
+    full_df = yf.download(ticker_list, start=start, end=end, interval=interval)
+    adj_close_df = full_df['Adj Close']
+    return adj_close_df
+
+# start='2018-01-01'
+# end='2020-12-31'
+# interval='1mo'
+# ticker_list = ['AAPL', 'NVDA', 'FTNT']
+
+# df = get_data(ticker_list, start, end, interval)
+
+
